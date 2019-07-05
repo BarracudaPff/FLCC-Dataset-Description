387344139793878b7c690f73bc2f5d4aff659906
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-07-05 21:36:23 +0300

    fx install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 jupyter labextension install jupyterlab-code-completion
-pip install jupyterlab-code-completion
+cd jupyterlab-code-completion
+pip3 install .
+cd ..
 jupyter serverextension enable --py jupyterlab-code-completion
