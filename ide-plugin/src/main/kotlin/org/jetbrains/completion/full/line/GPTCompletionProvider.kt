e730ae720cfbea8254b8f57d1b4ebf04f49bbfc0
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-13 15:57:23 +0300

    upd with new API

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/GPTCompletionProvider.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/GPTCompletionProvider.kt
@@ -1,25 +1,28 @@
 package org.jetbrains.completion.full.line
 
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.gson.Gson
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.io.HttpRequests
+import org.jetbrains.completion.full.line.models.FullLineCompletionRequest
+import org.jetbrains.completion.full.line.models.FullLineCompletionResult
 import java.util.concurrent.Callable
 
 class GPTCompletionProvider(private val url: String, private val port: Int) : FullLineCompletionProvider {
-    private companion object {
-        val RESULT_TYPE = object : TypeReference<List<String>>() {}
-    }
 
     override fun description(): String = "gpt"
 
     override fun getVariants(context: String): List<String> {
         return ApplicationManager.getApplication().executeOnPooledThread(Callable {
-            return@Callable HttpRequests.post("http://$url:$port/complete", "application/json").gzip(true)
+            return@Callable HttpRequests.post("http://$url:$port/completion/python3", "application/json").gzip(true)
                 .connect { r ->
-                    r.write("{ \"context\": \"${StringUtil.escapeStringCharacters(context)}\"}")
-                    ObjectMapper().readValue(r.inputStream, RESULT_TYPE) as List<String>
+                    val offset = context.length
+                    val token = StringUtil.getWordsIn(context).last()
+                    val request = FullLineCompletionRequest(StringUtil.escapeStringCharacters(context), token, offset)
+
+                    r.write(Gson().toJson(request))
+
+                    Gson().fromJson(r.reader, FullLineCompletionResult::class.java).completions
                 }
         }).get()
     }
