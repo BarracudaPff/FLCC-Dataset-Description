1c3e28d10a068395b81f39c3fa0605a72fcbca50
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-16 02:46:46 +0300

    rewrite how to call next level of completion using AnAction

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/CompletionTabListener.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/CompletionTabListener.kt
@@ -1,42 +1,43 @@
 package org.jetbrains.completion.full.line
 
-import com.intellij.codeInsight.completion.CodeCompletionHandlerBase
-import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.project.ProjectManager
-
+import com.intellij.util.messages.MessageBusConnection
 
 class CompletionTabListener : Disposable, AnActionListener {
     var nextLevel = false
-    private val idToCatch = "Choose Lookup Item Replace"
+    private var busConnection: MessageBusConnection = ApplicationManager
+            .getApplication()
+            .messageBus
+            .connect(this)
 
     init {
-        val busConnection = ApplicationManager.getApplication().messageBus.connect(this);
-        busConnection.subscribe(AnActionListener.TOPIC, this);
+        busConnection.subscribe(AnActionListener.TOPIC, this)
     }
 
     override fun afterActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
-        if (action.templateText == idToCatch) {
+        if (action.templateText == ID_TO_CATCH) {
             if (nextLevel) {
-                doNextLevel()
+                nextLevel = false
+                ActionManager.getInstance()
+                        .getAction(NEXT_LEVEL_ID)
+                        .actionPerformed(event)
             }
         }
     }
 
-    private fun doNextLevel() {
-        val project = ProjectManager.getInstance().openProjects[0]
-        val editor = FileEditorManager.getInstance(project).selectedTextEditor
-        CodeCompletionHandlerBase(CompletionType.BASIC, false, false, true)
-            .invokeCompletion(project, editor)
-        nextLevel = false
+    override fun dispose() {
+        busConnection.disconnect()
+        busConnection.dispose()
     }
 
-    override fun dispose() {
+    companion object {
+        const val ID_TO_CATCH = "Choose Lookup Item Replace"
+        const val NEXT_LEVEL_ID = "org.jetbrains.completion.full.line.NextLevelCompletionAction"
     }
 }
