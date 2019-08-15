78414c288bce1e9621bedd237c7f0c371b771cbb
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-08-15 16:53:51 +0300

    Remove dead code

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/DummyFullLineCompletionProvider.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.completion.full.line
-
-class DummyFullLineCompletionProvider : FullLineCompletionProvider {
-    override fun description(): String = "dummy"
-
-    override fun getVariants(context: String): List<String> = listOf("This is unbelievable!")
-}
