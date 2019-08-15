154c4a248068d8022c5ebf254d5a55390e4a5625
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-15 15:16:56 +0300

    add Insert plugin

---
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/MyInsertHandler.kt
@@ -0,0 +1,26 @@
+package org.jetbrains.completion.full.line
+
+import com.intellij.codeInsight.completion.InsertHandler
+import com.intellij.codeInsight.completion.InsertionContext
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.openapi.util.text.StringUtil
+
+class MyInsertHandler(
+    private val altHandler: CompletionTabListener = CompletionTabListener()
+) : InsertHandler<LookupElement> {
+    override fun handleInsert(context: InsertionContext, item: LookupElement) {
+        println(context.completionChar)
+        val ans = item.lookupString
+        val indexes = StringUtil.getWordIndicesIn(ans)
+        val offset = context.editor.caretModel.offset
+        if (indexes.size > 1) {
+            val token = ans.substring(0, indexes[1].startOffset)
+            context.editor.document.replaceString(offset - ans.length, offset, token)
+        }
+        if (context.completionChar != '\t') {
+            altHandler.nextLevel = true
+        }
+
+    }
+
+}
