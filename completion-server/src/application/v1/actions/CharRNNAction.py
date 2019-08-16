82fc5bb299b0eb70a1c941e9d30cc03681043158
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-16 18:21:56 +0300

    add Action for CharRNN

---
+++ completion-server/src/application/v1/actions/CharRNNAction.py
@@ -0,0 +1,41 @@
+import json
+
+from jsonschema import validate, ValidationError
+
+from src.application.v1.exceptions import ApiException
+from src.domain.services import CharRNNService
+
+# TODO(Move to config)
+schema = {
+    "type": "object",
+    "properties": {
+        "code": {"type": "string"},
+        "cursor_pos": {"type": "number"},
+        "filename": {"type": "string"},
+    },
+    "required": [
+        "filename",
+        "cursor_pos",
+        "code"
+    ]
+}
+
+
+def char_rnn_action(json_data: json, charRnnService: CharRNNService) -> json:
+    try:
+        validate(instance=json_data, schema=schema)
+
+        code = json_data["code"]
+        cursor_pos = json_data["cursor_pos"]
+        filename = json_data["filename"]
+
+        result = charRnnService.getCompletions(code, cursor_pos, filename)
+        return result
+
+    except ValidationError as e:
+        exception_data = {
+            "message": e.message,
+            "validator": e.validator,
+            "value": e.validator_value
+        }
+        raise ApiException("Wrong parameters passed to char rnn", e, exception_data, 500)
