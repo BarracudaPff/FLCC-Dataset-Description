200bf8d156f05ca5b3da1f0dd1e725febcc31eed
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-19 17:59:00 +0300

    upd module with new connector and new settings from config

--- completion-server/module.py
+++ completion-server/module.py
@@ -8,6 +8,7 @@ from src.application.v1.actions.GPTAction import GPTAction
 from src.application.v1.actions.StatusAction import StatusAction
 from src.domain.services.CharRNNService import CharRNNService
 from src.domain.services.GPTService import GPTService
+from src.infrastructure.connectors.CharRnnConnector import CharRnnConnector
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
@@ -15,18 +16,25 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 class AppModule(Module):
     def configure(self, binder):
         app = binder.injector.get(Flask)
-
         self.testing = app.testing
-        charModelPath = app.config['MODEL_CHAR_RNN_CPYTHON']
-        charParamPath = app.config['DATA_CHAR_RNN_CPYTHON']
-        charRnnService = self._configureCharRNNService(charParamPath, charModelPath)
 
-        gptModelPath = app.config['MODEL_GPT']
+        gptModelPath    = app.config['MODEL_GPT']
+        charModelPath   = app.config['MODEL_CHAR_RNN_CPYTHON']
+        charParamPath   = app.config['DATA_CHAR_RNN_CPYTHON']
+        maxBranches     = app.config['MAX_CHARS']
+        maxChars        = app.config['MAX_BRANCHES']
+
+        charRnnConnector = self._configureCharRNNService(charParamPath, charModelPath, maxBranches, maxChars)
+
         gptService = self._configureGPTService(gptModelPath)
+        charRnnService = CharRNNService(charRnnConnector)
 
-        charRnnAction = CharRnnAction(charRnnService)
-        gptAction = GPTAction(gptService)
         statusAction = StatusAction()
+        gptAction = GPTAction(gptService)
+        charRnnAction = CharRnnAction(charRnnService)
+
+        # Connectors
+        binder.bind(CharRnnConnector, to=charRnnConnector, scope=singleton)
 
         # Services
         binder.bind(GPTService      , to=gptService     , scope=singleton)
@@ -37,16 +45,16 @@ class AppModule(Module):
         binder.bind(GPTAction       , to=gptAction      , scope=singleton)
         binder.bind(CharRnnAction   , to=charRnnAction  , scope=singleton)
 
-    def _configureCharRNNService(self, params_path, model_path) -> CharRNNService:
+    def _configureCharRNNService(self, paramsPath, modelPath, maxBranches, maxChars) -> CharRnnConnector:
         # init path for parameters and model
-        params_path = os.path.join(ROOT, params_path)
-        model_path = os.path.join(ROOT, model_path)
+        paramsPath = os.path.join(ROOT, paramsPath)
+        modelPath = os.path.join(ROOT, modelPath)
 
         # TODO (Download model here if we want)
         if self.testing:
-            assert os.path.exists(params_path) and os.path.exists(model_path), \
+            assert os.path.exists(paramsPath) and os.path.exists(modelPath), \
                 'Params and model not found. See README.md to download them'
-        return CharRNNService(params_path, model_path)
+        return CharRnnConnector(paramsPath, modelPath, maxBranches, maxChars)
 
     def _configureGPTService(self, modelPath) -> GPTService:
         modelPath = os.path.join(ROOT, modelPath)
