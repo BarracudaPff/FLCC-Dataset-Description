bf8d81367d20d31412e5057f2560295d4978c845
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-20 06:52:14 +0300

    use json config

--- completion-server/app.py
+++ completion-server/app.py
@@ -7,7 +7,7 @@ from module import AppModule
 from src.application.v1.api import api
 
 app = Flask(__name__)
-app.config.from_pyfile('config.cfg')
+app.config.from_json('config.json')
 
 logging.basicConfig(format="%(asctime)s\t:\t%(levelname)s\t:\t%(name)s\t:\t%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
