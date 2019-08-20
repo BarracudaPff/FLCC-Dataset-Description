72d9fb73c12a34d02ac6c3600c8e63f6c61f271d
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-20 08:23:56 +0300

    add slashes for names

--- completion-server/src/infrastructure/connectors/__init__.py
+++ completion-server/src/infrastructure/connectors/__init__.py
@@ -4,8 +4,8 @@ from abc import ABC, abstractmethod
 class Connector(ABC):
     @abstractmethod
     def __init__(self, model_name):
-        params = "models/data" + model_name
-        model = "models" + model_name
+        params = "models/data/" + model_name
+        model = "models/" + model_name
         self.connect(params, model)
 
     @abstractmethod
