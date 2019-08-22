d5d22366156b24203d8577d6c76a881399c26306
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-22 16:51:28 +0300

    add timeout for >=10 seconds and catch exceptions

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/NetworkCompletionProvider.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/NetworkCompletionProvider.kt
@@ -7,22 +7,27 @@ import com.intellij.util.io.HttpRequests
 import org.jetbrains.completion.full.line.models.FullLineCompletionRequest
 import org.jetbrains.completion.full.line.models.FullLineCompletionResult
 import java.util.concurrent.Callable
+import java.util.concurrent.TimeUnit
 
 class NetworkCompletionProvider(override val description: String, private val url: String) :
     FullLineCompletionProvider {
     override fun getVariants(context: String, filename: String): List<String> {
-        return ApplicationManager.getApplication().executeOnPooledThread(Callable {
-            return@Callable HttpRequests.post(url, "application/json").gzip(true)
-                .connect { r ->
-                    println(filename)
-                    val offset = context.length
-                    val token = StringUtil.getWordsIn(context).last()
-                    val request = FullLineCompletionRequest(context, token, offset, filename)
+        return try {
+            ApplicationManager.getApplication().executeOnPooledThread(Callable {
+                return@Callable HttpRequests.post(url, "application/json").gzip(true)
+                    .connect { r ->
+                        println(filename)
+                        val offset = context.length
+                        val token = StringUtil.getWordsIn(context).last()
+                        val request = FullLineCompletionRequest(context, token, offset, filename)
 
-                    r.write(Gson().toJson(request))
+                        r.write(Gson().toJson(request))
 
-                    Gson().fromJson(r.reader, FullLineCompletionResult::class.java).completions
-                }
-        }).get()
+                        Gson().fromJson(r.reader, FullLineCompletionResult::class.java).completions
+                    }
+            }).get(10, TimeUnit.SECONDS)
+        } catch (e: Exception) {
+            emptyList()
+        }
     }
 }
