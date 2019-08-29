f4c8f369224a7496ad2cd4368e730820581cb8ce
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-08-29 22:40:01 +0300

    add assistant file to count siva in temporary folders

---
+++ dataset/scripts/count-siva.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "/tmp/root-repositories/"
+ls /tmp/root-repositories/ | wc -l
+echo "/tmp/root-repositories-1/"
+ls /tmp/root-repositories-1/ | wc -l
+echo "/tmp/root-repositories-2/"
+ls /tmp/root-repositories-2/ | wc -l
+echo "/tmp/root-repositories-3/"
+ls /tmp/root-repositories-3/ | wc -l
