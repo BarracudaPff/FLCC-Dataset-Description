2dc14bbcf9917c5b80112954ea7d84dae2d7063d
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-09-12 11:06:01 +0300

    add config for better configuration cases

---
+++ dataset/src/config.py
@@ -0,0 +1,28 @@
+import os
+
+statistic_folder = 'statistics'
+dataDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data/')
+max_files_in_folder = 100
+
+# Temporary fields
+temp_folder = 'temp'
+list_siva = 'siva.txt'
+list_siva_temp = 'list-siva-temp.txt'
+temp_repo_list = 'data/temp-repo-list.txt'
+
+hide_output = ' > /dev/null'
+
+# TODO: add password and email here for email logging
+password = "None"
+sender_email = "None"
+subject = '''\
+Subject: Dataset info.
+
+>'''
+
+# PSQL For Borges
+db_user = "testing"
+db_password = "testing"
+db_host = "127.0.0.1"
+db_port = 5432
+db_database = "testing"
