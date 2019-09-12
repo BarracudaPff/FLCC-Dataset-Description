b375cb55e7d78089dc94fb4db47f90ad74b3ef83
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-09-12 11:05:37 +0300

    simplify dumping errors from PostgreSQL

--- dataset/src/dump_error_reps.py
+++ dataset/src/dump_error_reps.py
@@ -1,52 +1,30 @@
-import argparse
-
 import psycopg2
 
-from src.mail import MailNotifier
-
-subject = '''\
-Subject: Dataset info From AWS Machine. Repositories with errors
-
->'''
-
-parser = argparse.ArgumentParser(description='Dump skipped repositories from borges and PostgreSQL')
-parser.add_argument('--target_directory', type=str, help='Path to file with skipped repositories',
-                    default='data/skipped.txt')
-parser.add_argument('--email_notify', type=str, help='Gmail to notify. Type none to skip', default='none')
+from src.config import subject, db_user, db_password, db_host, db_port, db_database
+from src.utils.files import writeLinesFile
+from src.utils.mail import MailNotifier
 
 
-def main(email_notify, target_directory):
-    try:
-        select_query = "SELECT (endpoints) FROM repositories WHERE status != 'fetched';"
+def dump_errors(email_notify, skipped_filename):
+    select_query = "SELECT (endpoints) FROM repositories WHERE status != 'fetched';"
 
-        connection = psycopg2.connect(user="testing",
-                                      password="testing",
-                                      host="127.0.0.1",
-                                      port="5432",
-                                      database="testing")
-        cursor = connection.cursor()
-        cursor.execute(select_query)
-        records = cursor.fetchall()
+    connection = psycopg2.connect(user=db_user,
+                                  password=db_password,
+                                  host=db_host,
+                                  port=db_port,
+                                  database=db_database)
+    cursor = connection.cursor()
+    cursor.execute(select_query)
+    records = cursor.fetchall()
 
-        git_repos = []
-        for row in records:
-            git_repos.append(row[0][0] + '\n')
+    git_repos = [row[0][0] for row in records]
 
-        MailNotifier(subject, email_notify).send_notification(
-            f"Skipped: {git_repos}\nTotal {git_repos.__len__()} repos")
-        with open(target_directory, 'a') as f:
-            f.writelines(git_repos)
+    writeLinesFile(skipped_filename, git_repos, mode='a', appendWithNewLine=True)
 
-        # closing database connection.
-        if connection:
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-    except Exception as e:
-        if email_notify != 'none':
-            MailNotifier(subject, email_notify).send_error(e)
+    MailNotifier(subject, email_notify).send_notification(
+        f"Skipped {len(git_repos)} repositories: {git_repos}")
 
-
-if __name__ == '__main__':
-    args = parser.parse_args()
-    main(args.email_notify, args.target_directory)
+    # closing database connection.
+    if connection:
+        cursor.close()
+        connection.close()
