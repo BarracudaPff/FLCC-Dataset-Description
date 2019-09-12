3279d6b04967cbc1f66d2717a083d11970f99f33
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-09-12 11:02:21 +0300

    move exts.py to utils, now need to pass language filename.

---
+++ dataset/src/utils/exts.py
@@ -0,0 +1,38 @@
+import yaml
+
+from .files import writeJsonFile, readLinesFile
+from .web import downloadWithUrl
+
+
+def extensions(languages_file):
+    if languages_file is None:
+        languages, data = _getAllLanguages()
+    else:
+        languages = readLinesFile(languages_file)
+        _, data = _getAllLanguages()
+
+    _extensions = {}
+
+    for language in languages:
+        try:
+            languageExtensions = data.get(language).get('extensions')
+            if languageExtensions is None:
+                continue
+            for extension in languageExtensions:
+                _extensions[extension] = f"{language}/{extension}/0"
+        except AttributeError:
+            raise TypeError(f"Wrong language name: {language}")
+
+    writeJsonFile("extensions.json", _extensions)
+    print(f"Initialized {len(_extensions)} extensions for {len(languages)} languages")
+    return _extensions
+
+
+def _getAllLanguages() -> ([], dict):
+    content = downloadWithUrl(
+        'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml',
+        'languages.yml',
+        False)
+    data = yaml.safe_load(content)
+    languages = [language for language in data]
+    return languages, data
