9e76b342f2a331f6c5381c65d7a3f33134c6c06c
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-09-16 21:36:21 +0300

    update dumping errors with new db file

--- dataset/src/dump_error_reps.py
+++ dataset/src/dump_error_reps.py
@@ -1,30 +1,14 @@
-import psycopg2
-
-from src.config import subject, db_user, db_password, db_host, db_port, db_database
+from src.config import subject
+from src.utils.db import Connector
 from src.utils.files import writeLinesFile
 from src.utils.mail import MailNotifier
 
 
 def dump_errors(email_notify, skipped_filename):
-    select_query = "SELECT (endpoints) FROM repositories WHERE status != 'fetched';"
+    with Connector() as connector:
+        git_repos = connector.get_repositories_with_errors()
 
-    connection = psycopg2.connect(user=db_user,
-                                  password=db_password,
-                                  host=db_host,
-                                  port=db_port,
-                                  database=db_database)
-    cursor = connection.cursor()
-    cursor.execute(select_query)
-    records = cursor.fetchall()
+        writeLinesFile(skipped_filename, git_repos, mode='a', appendWithNewLine=True)
 
-    git_repos = [row[0][0] for row in records]
-
-    writeLinesFile(skipped_filename, git_repos, mode='a', appendWithNewLine=True)
-
-    MailNotifier(subject, email_notify).send_notification(
-        f"Skipped {len(git_repos)} repositories: {git_repos}")
-
-    # closing database connection.
-    if connection:
-        cursor.close()
-        connection.close()
+        MailNotifier(subject, email_notify).send_notification(
+            f"Skipped {len(git_repos)} repositories: {git_repos}")
