8ab1b98b248b6627554b1c4256ab1ad156a68600
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-10-02 11:42:20 +0300

    add exception handler and use new method to get last token

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/NetworkCompletionProvider.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/NetworkCompletionProvider.kt
@@ -3,42 +3,49 @@ package org.jetbrains.completion.full.line
 import com.google.gson.Gson
 import com.intellij.openapi.application.ex.ApplicationUtil
 import com.intellij.openapi.progress.EmptyProgressIndicator
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.io.HttpRequests
 import org.jetbrains.completion.full.line.FullLineContributor.Companion.LOG
 import org.jetbrains.completion.full.line.models.FullLineCompletionRequest
 import org.jetbrains.completion.full.line.models.FullLineCompletionResult
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 import java.util.concurrent.Callable
 import java.util.concurrent.ExecutionException
 
 class NetworkCompletionProvider(override val description: String, private val url: String) :
-    FullLineCompletionProvider {
+        FullLineCompletionProvider {
     override fun getVariants(context: String, filename: String): List<String> {
         return try {
             val start = System.currentTimeMillis()
             val completions = ApplicationUtil.runWithCheckCanceled(Callable {
                 return@Callable HttpRequests.post(url, "application/json").gzip(true)
-                    .connect { r ->
-                        val offset = context.length
-                        val token = StringUtil.getWordsIn(context).last()
-                        val request = FullLineCompletionRequest(context, token, offset, filename)
+                        .connect { r ->
+                            val offset = context.length
+                            val token = TextUtils.getLastToken(context)
 
-                        r.write(Gson().toJson(request))
+                            val request = FullLineCompletionRequest(context, token, offset, filename)
+                            r.write(Gson().toJson(request))
 
-                        Gson().fromJson(r.reader, FullLineCompletionResult::class.java).completions
-                    }
+                            Gson().fromJson(r.reader, FullLineCompletionResult::class.java).completions
+                        }
             }, EmptyProgressIndicator())
-            LOG.debug("Time to predict completions is ${System.currentTimeMillis() - start}")
+            LOG.debug("Time to predict completions is ${(System.currentTimeMillis() - start) / 1000}")
             completions
-        } catch (e: HttpRequests.HttpStatusException) {
-            logError("Something wrong with completion server. $e", e)
-        } catch (e: ExecutionException) {
-            logError("Error while getting completions from server", e)
+
+        } catch (e: Exception) {
+            val message = when (e) {
+                is SocketTimeoutException                       -> "Timeout. Probably IP is wrong"
+                is ConnectException                             -> "Network is down. Can't reach server"
+                is HttpRequests.HttpStatusException             -> "Something wrong with completion server"
+                is ExecutionException, is IllegalStateException -> "Error while getting completions from server"
+                else                                            -> "Some other error occurred"
+            }
+            return logError(message, e)
         }
     }
 
     private fun logError(msg: String, error: Exception): List<String> {
-        LOG.error(msg, error)
+        println("$error. $msg")
         return emptyList()
     }
 }
