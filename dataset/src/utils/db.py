0e8f724ef64f83e2564c4cf8079a63ab0ae983e7
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-10-22 08:51:31 +0300

    remove method get_repository_by_siva()

--- dataset/src/utils/db.py
+++ dataset/src/utils/db.py
@@ -9,18 +9,6 @@ class Connector:
     def __init__(self, user=db_user, password=db_password, host=db_host, port=db_port):
         self.connection = psycopg2.connect(user=user, password=password, host=host, port=port)
 
-    def get_repository_by_siva(self, siva: str) -> []:
-        select_query = f"SELECT repository_references.init, repositories.endpoints " \
-            f"FROM repository_references INNER JOIN repositories " \
-            f"ON repository_references.repository_id = repositories.id " \
-            f"WHERE repository_references.init ='{siva}' GROUP BY repository_references.init, endpoints"
-        # return f"dev/null/{str(uuid.uuid4())}"
-        records = self._records(select_query)
-        try:
-            return f"{records[0][1][0][19:]}/{records[0][0]}"
-        except Exception as e:
-            return None
-
     def get_repositories_with_errors(self) -> []:
         select_query = "SELECT (endpoints) FROM repositories WHERE status != 'fetched';"
 
