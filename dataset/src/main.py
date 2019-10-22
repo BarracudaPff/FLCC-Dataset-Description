c3dcb9ec08b486cae0f42bea56c30a8c306cc708
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-10-22 08:56:34 +0300

    add some arguments to usbparsers

--- dataset/src/main.py
+++ dataset/src/main.py
@@ -5,9 +5,9 @@ import main
 from calibrate_dataset import calibrate
 from config import subject, dataDir
 from dump_error_reps import dump_errors
-from get_dataset import Dataset
+from get_dataset import Dataset, modes
 from prepare_repo_list import prepare_list
-from statistic import statistic
+from statistic import Statistic
 from utils.exts import extensions
 from utils.mail import MailNotifier
 
@@ -31,7 +31,8 @@ base_parser.add_argument('--email_notify', type=str, help='Gmail to notify. Type
 subparsers = parser.add_subparsers(dest='subparser')
 
 # A dataset command
-dataset_parser = subparsers.add_parser('dataset', help='Get dataset for Languages', parents=[base_parser])
+dataset_parser = subparsers.add_parser('dataset', help='Get dataset for all languages and all extension',
+                                       parents=[base_parser])
 dataset_parser.add_argument('--sivas_folder', type=str, default='/tmp/root-repositories/',
                             help='Directory where are sivas')
 dataset_parser.add_argument('--slice', type=int, default=500,
@@ -40,10 +41,20 @@ dataset_parser.add_argument('--languages_file', type=str, default='languages.txt
                             help='Path to file with list of languages')
 dataset_parser.add_argument('-mode', type=str, action="store", choices=['pga', 'borges'], required=True,
                             help='Use PGA')
+dataset_parser.add_argument('--only_master', type=bool, default=False,
+                            help='TODO')
+dataset_parser.add_argument('--additional', type=str, action="append", nargs='*', choices=modes, default=[modes],
+                            help='TODO')
 
 # A statistic command
 statistic_parser = subparsers.add_parser('statistic', help='Get statistic for downloaded dataset',
                                          parents=[base_parser])
+statistic_parser.add_argument('--languages_file', type=str, default='languages.txt',
+                              help='Path to file with list of languages')
+statistic_parser.add_argument('--modes', type=str, action="append", nargs='*',
+                              choices=['languages', 'extensions', 'repositories'],
+                              default=[['languages', 'extensions', 'repositories']],
+                              help='TODO')
 
 # A prepare list command
 list_parser = subparsers.add_parser('prepare_list', help='Extract slice of repositories to downloading and compress',
@@ -61,18 +72,14 @@ errors_parser.add_argument('--skipped_file', type=str, default='skipped.txt',
 
 # A calibrate command
 calibrate_parser = subparsers.add_parser('calibrate', help='Calibrate dataset')
+calibrate_parser.add_argument('--only_master', type=bool, default=False,
+                              help='TODO')
 
 
 def _dataset(args: argparse.Namespace):
-    languages_file = args.languages_file
+    exts = get_extensions(args.languages_file)
 
-    if os.path.exists(dataDir + languages_file):
-        exts = extensions(args.languages_file)
-    else:
-        print(f"File {languages_file} with language list wasn't found. Downloading all possible languages")
-        exts = extensions(None)
-
-    dataset = Dataset(exts, args.slice, args.target_directory, args.email_notify)
+    dataset = Dataset(exts, args.slice, args.target_directory, args.email_notify, args.additional[0], args.only_master)
     if args.mode == 'pga':
         dataset.pga()
     elif args.mode == 'borges':
@@ -82,7 +89,9 @@ def _dataset(args: argparse.Namespace):
 
 
 def _statistic(args: argparse.Namespace):
-    statistic(args.email_notify, args.target_directory)
+    exts = get_extensions(args.languages_file)
+    stats = Statistic(args.email_notify, args.target_directory)
+    stats.gather_statistic(args.modes, args.languages_file, exts)
 
 
 def _prepare_list(args: argparse.Namespace):
@@ -94,7 +103,17 @@ def _dump_errors(args: argparse.Namespace):
 
 
 def _calibrate(args: argparse.Namespace):
-    calibrate(args.email_notify, args.target_directory)
+    calibrate(args.email_notify, args.target_directory, args.only_master)
+
+
+def get_extensions(languages_file):
+    if os.path.exists(dataDir + languages_file):
+        exts = extensions(languages_file)
+    else:
+        print(f"File {languages_file} with language list wasn't found. Downloading all possible languages")
+        exts = extensions(None)
+
+    return exts
 
 
 if __name__ == '__main__':
