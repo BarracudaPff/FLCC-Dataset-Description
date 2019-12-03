6cb800a51514ecb849e229d865ffbdee037cb11a
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-12-03 05:30:25 +0300

    upd Request model with new api and settings

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/models/FullLineCompletionRequest.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/models/FullLineCompletionRequest.kt
@@ -1,8 +1,32 @@
 package org.jetbrains.completion.full.line.models
 
+import com.google.gson.annotations.SerializedName
+import org.jetbrains.completion.full.line.settings.MLServerCompletionSettings
+
 data class FullLineCompletionRequest(
-    val code: String,
-    val token: String,
-    val offset: Int,
-    val filename: String
-)
+        val code: String,
+        val token: String,
+        val offset: Int,
+        val filename: String,
+        val branches: Int,
+
+        val tokens: Int,
+        val mode: FullLineCompletionMode,
+        val generator: FullLineCompletionAlgorithm,
+
+        @SerializedName("beam_size")
+        val beamSize: Int,
+
+        @SerializedName("diversity_strength")
+        val diversityStrength: Double,
+
+        @SerializedName("top_n")
+        val topN: Int,
+        @SerializedName("use_top_n")
+        val useTopN: Boolean
+
+) {
+    constructor(code: String, prefix: String, offset: Int, filename: String, settings: MLServerCompletionSettings)
+            : this(code, prefix, offset, filename, settings.suggestions, settings.tokens, settings.mode,
+            settings.algorithm, settings.beamSize, settings.diversityStrength, settings.topN, settings.useTopN)
+}
