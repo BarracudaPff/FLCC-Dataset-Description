af9c22f129d6115e8f85607615e3470005a2b0c4
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-12-05 17:56:14 +0300

    do some fixes

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/NetworkCompletionProvider.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/NetworkCompletionProvider.kt
@@ -1,8 +1,8 @@
 package org.jetbrains.completion.full.line
 
 import com.google.gson.Gson
+import com.intellij.codeInsight.completion.impl.CompletionServiceImpl
 import com.intellij.openapi.application.ex.ApplicationUtil
-import com.intellij.openapi.progress.EmptyProgressIndicator
 import com.intellij.util.io.HttpRequests
 import org.jetbrains.completion.full.line.FullLineContributor.Companion.LOG
 import org.jetbrains.completion.full.line.models.FullLineCompletionRequest
@@ -28,14 +28,15 @@ class NetworkCompletionProvider(override val description: String, private val ur
 
                             Gson().fromJson(r.reader, FullLineCompletionResult::class.java).completions
                         }
-            }, EmptyProgressIndicator())
+                //ProgressIndicatorProvider.getInstance().progressIndicator
+            }, CompletionServiceImpl.getCurrentCompletionProgressIndicator())
             LOG.debug("Time to predict completions is ${(System.currentTimeMillis() - start) / 1000}")
             completions
 
         } catch (e: Exception) {
             val message = when (e) {
+                is ConnectException                             -> return emptyList()
                 is SocketTimeoutException                       -> "Timeout. Probably IP is wrong"
-                is ConnectException                             -> "Network is down. Can't reach server"
                 is HttpRequests.HttpStatusException             -> "Something wrong with completion server"
                 is ExecutionException, is IllegalStateException -> "Error while getting completions from server"
                 else                                            -> "Some other error occurred"
