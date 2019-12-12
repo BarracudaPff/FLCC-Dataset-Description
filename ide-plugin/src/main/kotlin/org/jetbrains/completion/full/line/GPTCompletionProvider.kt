2534a8b8da0c4db0516bb10d1e011e1b6b3f1373
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-12-12 15:12:41 +0300

    rewrite with ThreadPoolExecutor

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/GPTCompletionProvider.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/GPTCompletionProvider.kt
@@ -3,47 +3,43 @@ package org.jetbrains.completion.full.line
 import com.google.gson.Gson
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.progress.util.ProgressIndicatorUtils
-import com.intellij.util.concurrency.SequentialTaskExecutor
+import com.intellij.util.ConcurrencyUtil
 import com.intellij.util.io.HttpRequests
 import org.jetbrains.completion.full.line.models.FullLineCompletionRequest
 import org.jetbrains.completion.full.line.models.FullLineCompletionResult
 import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.util.concurrent.ExecutionException
-import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.Future
 
 class GPTCompletionProvider(private val host: String, private val port: Int) {
     val description = "  gpt"
-    private val lock = ReentrantLock()
 
     fun getVariants(context: String, filename: String): List<String> {
         val start = System.currentTimeMillis()
-        val future = SequentialTaskExecutor.createSequentialApplicationPoolExecutor(javaClass.simpleName)
-                .submit<List<String>> {
-                    lock.withListLock {
-                        try {
-                            HttpRequests.post("http://$host:$port/v1/complete/gpt", "application/json")
-                                    .connect { r ->
-                                        val offset = context.length
-                                        val token = TextUtils.getLastToken(context)
+        val future = submitPool {
+            try {
+                HttpRequests.post("http://$host:$port/v1/complete/gpt", "application/json")
+                        .connect { r ->
+                            val offset = context.length
+                            val token = TextUtils.getLastToken(context)
 
-                                        val request = FullLineCompletionRequest(context, token, offset, filename)
-                                        r.write(Gson().toJson(request))
+                            val request = FullLineCompletionRequest(context, token, offset, filename)
+                            r.write(Gson().toJson(request))
 
-                                        Gson().fromJson(r.reader, FullLineCompletionResult::class.java).completions
-                                    }
-                        } catch (e: Exception) {
-                            val message = when (e) {
-                                is ConnectException                             -> return@submit emptyList()
-                                is SocketTimeoutException                       -> "Timeout. Probably IP is wrong"
-                                is HttpRequests.HttpStatusException             -> "Something wrong with completion server"
-                                is ExecutionException, is IllegalStateException -> "Error while getting completions from server"
-                                else                                            -> "Some other error occurred"
-                            }
-                            logError(message, e)
+                            Gson().fromJson(r.reader, FullLineCompletionResult::class.java).completions
                         }
-                    }
+            } catch (e: Exception) {
+                val message = when (e) {
+                    is ConnectException                             -> return@submitPool emptyList()
+                    is SocketTimeoutException                       -> "Timeout. Probably IP is wrong"
+                    is HttpRequests.HttpStatusException             -> "Something wrong with completion server"
+                    is ExecutionException, is IllegalStateException -> "Error while getting completions from server"
+                    else                                            -> "Some other error occurred"
                 }
+                logError(message, e)
+            }
+        }
 
         ProgressIndicatorUtils.awaitWithCheckCanceled(future)
 
@@ -56,20 +52,16 @@ class GPTCompletionProvider(private val host: String, private val port: Int) {
         return emptyList()
     }
 
-    private inline fun <T> ReentrantLock.withListLock(action: () -> List<T>): List<T> {
-        if (isLocked) {
-            return emptyList()
+    private fun submitPool(task: () -> List<String>): Future<List<String>> {
+        if (executor.queue.size > 0) {
+            executor.queue.poll()
         }
-
-        lock()
-        try {
-            return action()
-        } finally {
-            unlock()
-        }
+        return executor.submit(task)
     }
 
     companion object {
         private val LOG = Logger.getInstance(FullLineContributor::class.java)
+
+        private val executor = ConcurrencyUtil.newSingleThreadExecutor(FullLineContributor::class.java.simpleName)
     }
 }
