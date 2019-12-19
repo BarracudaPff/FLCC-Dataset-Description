93e821af8cf296e09c9a48e74956b6a31fbd6a85
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-12-19 11:56:27 +0300

    remove "testing" settings from panel, upd messages, set settings in 2 modes, add option for showing only Top N suggestions

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/settings/MLServerCompletionConfigurable.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/settings/MLServerCompletionConfigurable.kt
@@ -5,7 +5,6 @@ import com.intellij.openapi.options.SearchableConfigurable
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.ui.layout.*
-import org.jetbrains.completion.full.line.models.FullLineCompletionAlgorithm
 import org.jetbrains.completion.full.line.models.FullLineCompletionMode
 import org.jetbrains.completion.full.line.settings.MLServerCompletionBundle.Companion.message
 import java.awt.event.ActionEvent
@@ -13,14 +12,14 @@ import java.awt.event.ActionEvent
 class MLServerCompletionConfigurable(private val event: (event: ActionEvent) -> Unit)
     : BoundConfigurable("Server Code Completion"), SearchableConfigurable {
     private lateinit var gpt: ComponentPredicate
-    private val settings = MLServerCompletionSettings.getInstance()
+    private val settings = MLServerCompletionSettings.getInstance().state
 
     override fun createPanel(): DialogPanel {
         return panel {
             titledRow(message("ml.server.completion.settings.group")) {
                 row {
                     cell {
-                        gpt = checkBox(message("ml.server.completion.gpt"), settings::enable).selected
+                        gpt = checkBox(message("ml.server.completion.title"), settings::enable).selected
                         button("Test Connection", actionListener = event)
                     }
                     row {
@@ -33,55 +32,31 @@ class MLServerCompletionConfigurable(private val event: (event: ActionEvent) -> 
                         }
 
                         row {
-                            checkBox(message("ml.server.completion.autoPopup"), settings::autoPopup, message("ml.server.completion.autoPopup.desc"))
+                            checkBox(message("ml.server.completion.autoPopup"), settings::autoPopup)
+                        }
+                        row {
+                            val use = checkBox(message("ml.server.completion.top.n.use")).selected
+                            row { intTextFieldFixed(settings::topN, 1, IntRange(0, 100)) }.enableIf(use)
                         }
 
                         if (Registry.get("ml.server.completion.expand.settings").asBoolean()) {
-                            row {
-                                buttonGroup(settings::algorithm) {
-                                    label(message("ml.server.completion.algorithm"))
-                                    row {
-                                        radioButton(message("ml.server.completion.algorithm.df"), FullLineCompletionAlgorithm.DEFAULT)
-                                        row(message("ml.server.completion.tokens")) {
-                                            intTextFieldFixed(settings::tokens, 1, IntRange(0, 100))
-                                        }
-                                        row(message("ml.server.completion.suggestions")) {
-                                            intTextFieldFixed(settings::suggestions, 1, IntRange(0, 100))
-                                        }
-                                    }
-
-                                    row {
-                                        radioButton(message("ml.server.completion.algorithm.bs"), FullLineCompletionAlgorithm.BEAM_SEARCH)
-                                        row(message("ml.server.completion.num.iterations")) {
-                                            intTextFieldFixed(settings::numIterations, 1, IntRange(0, 100))
-                                        }
-                                        row(message("ml.server.completion.beam.size")) {
-                                            intTextFieldFixed(settings::beamSize, 1, IntRange(0, 20))
-                                        }
-                                        row(message("ml.server.completion.diversity.groups")) {
-                                            intTextFieldFixed(settings::diversityGroups, 1, IntRange(0, 20))
-                                        }
-                                        row(message("ml.server.completion.diversity.strength")) {
-                                            doubleTextField(settings::diversityStrength, 1, IntRange(0, 10))
-                                        }
-                                        row {
-                                            val use = checkBox(message("ml.server.completion.topn.use"), settings::useTopN).selected
-                                            row { intTextFieldFixed(settings::topN, 1, IntRange(0, 100)) }.enableIf(use)
-                                        }
-                                    }
-
-                                    row {
-                                        radioButton(message("ml.server.completion.algorithm.viterbi"), FullLineCompletionAlgorithm.VITERBI)
-                                    }.enabled = false
+                            row(message("ml.server.completion.bs")) {
+                                row(message("ml.server.completion.bs.num.iterations")) {
+                                    intTextFieldFixed(settings::numIterations, 1, IntRange(0, 50))
                                 }
-                            }
-
-                            row {
-                                checkBox(message("ml.server.completion.filter"), settings::filter)
-                            }
-
-                            row {
-                                checkBox(message("ml.server.completion.experimental"), settings::experimental)
+                                row(message("ml.server.completion.bs.beam.size")) {
+                                    intTextFieldFixed(settings::beamSize, 1, IntRange(0, 20))
+                                }
+                                row(message("ml.server.completion.bs.diversity.strength")) {
+                                    doubleTextField(settings::diversityStrength, 1, IntRange(0, 1))
+                                }
+                                row(message("ml.server.completion.bs.diversity.groups")) {
+                                    intTextFieldFixed(settings::diversityGroups, 1, IntRange(0, 10))
+                                    row {
+                                        val groupUse = checkBox(message("ml.server.completion.group.top.n.use")).selected
+                                        row { intTextFieldFixed(settings::groupTopN, 1, IntRange(0, 100)) }.enableIf(groupUse)
+                                    }
+                                }
                             }
                         }
                     }.enableSubRowsIf(gpt)
