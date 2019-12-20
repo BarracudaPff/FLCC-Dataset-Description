415f4cb72e106a4822e1b69595d9ff4417dd47ab
BarracudaPff <Kirill.Krylov@jetbrains.com>
2019-12-20 11:22:35 +0300

    fx spacing

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/settings/MLServerCompletionUI.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/settings/MLServerCompletionUI.kt
@@ -7,8 +7,8 @@ import com.intellij.ui.layout.toBinding
 import javax.swing.JTextField
 import kotlin.reflect.KMutableProperty0
 
-//For some reason method intTextField() in com/intellij/ui/layout/Cell.kt throws
-// java.lang.LinkageError: loader constraint violation: when resolving method,
+// For some reason method intTextField() in com/intellij/ui/layout/Cell.kt
+// throws java.lang.LinkageError: loader constraint violation: when resolving method,
 // But it's copy works fine :/
 fun Row.intTextFieldFixed(prop: KMutableProperty0<Int>, columns: Int? = null, range: IntRange? = null): CellBuilder<JTextField> {
     val binding = prop.toBinding()
