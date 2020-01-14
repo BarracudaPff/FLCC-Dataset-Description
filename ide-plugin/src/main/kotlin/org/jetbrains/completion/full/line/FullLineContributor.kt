b5b94879bcbfbbe090929a6491db1c13628d3c90
BarracudaPff <Kirill.Krylov@jetbrains.com>
2020-01-14 17:10:14 +0300

    divide logic into small methods

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/FullLineContributor.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/FullLineContributor.kt
@@ -6,13 +6,9 @@ import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.util.elementType
 import com.jetbrains.python.PyTokenTypes
 import icons.PythonIcons
-import org.jetbrains.completion.full.line.handlers.FullLineInsertHandler
-import org.jetbrains.completion.full.line.handlers.OneTokenHandler
 import org.jetbrains.completion.full.line.language.LanguageMLSupporter
 import org.jetbrains.completion.full.line.models.FullLineCompletionMode
 import org.jetbrains.completion.full.line.services.NextLevelFullLineCompletion
@@ -20,42 +16,29 @@ import org.jetbrains.completion.full.line.settings.MLServerCompletionSettings
 import javax.swing.Icon
 
 class FullLineContributor : CompletionContributor() {
-    companion object {
-        const val FULL_LINE_TAIL_TEXT = "full-line"
-        private val settings = MLServerCompletionSettings.getInstance()
-
-        val LOG = Logger.getInstance(FullLineContributor::class.java)
-
-        val INSERT_HANDLER = FullLineInsertHandler()
-    }
-
     private val provider = GPTCompletionProvider()
-    private val provider = GPTCompletionProvider(host, port)
     private val settings = MLServerCompletionSettings.getInstance()
     private val service: NextLevelFullLineCompletion = ServiceManager.getService(NextLevelFullLineCompletion::class.java)
 
     override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
-        if (!settings.enable
-                || (parameters.isAutoPopup && !settings.autoPopup)
-                || parameters.position.elementType == PyTokenTypes.END_OF_LINE_COMMENT)
-        if (!settings.isEnabled())
+        if (!settings.isEnabled()
+                || (parameters.isAutoPopup && !settings.isAutoPopup())
+                || (parameters.position.elementType == PyTokenTypes.END_OF_LINE_COMMENT) && settings.enableComments())
             return
 
         val supporter = LanguageMLSupporter.getInstance(parameters.originalFile.language) ?: return
 
         handleFirstLine(result, supporter)
 
-        val context = parameters.originalFile.text.substring(0, parameters.offset)
+        val context = parameters.originalFile.text
         val filename = parameters.originalFile.name
-        val prefix = supporter.getPrefix(context)
+        val offset = parameters.offset
 
-        for (variant in provider.getVariants(context, filename, prefix)) {
-            if (settings.filter && !supporter.isCorrect(variant)) {
-                continue
-            }
+        val prefix = supporter.getPrefix(context.substring(0, parameters.offset))
 
-            val lookupElementBuilder = getLookupElementBuilder(supporter, variant) ?: continue
-            result.addElement(PrioritizedLookupElement.withPriority(lookupElementBuilder, 100000.0))
+        for (variant in provider.getVariants(context, filename, prefix, offset)) {
+            val element = getLookupElementBuilder(supporter, variant) ?: continue
+            result.addElement(PrioritizedLookupElement.withPriority(element, 100000.0))
         }
     }
 
@@ -73,10 +56,9 @@ class FullLineContributor : CompletionContributor() {
     }
 
     private fun getLookupElementBuilder(supporter: LanguageMLSupporter, variant: String): LookupElementBuilder? {
-        return if (settings.mode == FullLineCompletionMode.ONE_TOKEN) {
+        return if (settings.state.mode == FullLineCompletionMode.ONE_TOKEN) {
             val token = supporter.getFirstToken(variant) ?: return null
             LookupElementBuilder.create(token)
-                    .withInsertHandler(OneTokenHandler())
         } else {
             LookupElementBuilder.create(variant)
                     .withTypeText(FULL_LINE_TAIL_TEXT)
@@ -85,11 +67,6 @@ class FullLineContributor : CompletionContributor() {
     }
 
     companion object {
-        val LOG = Logger.getInstance(FullLineContributor::class.java)
-
-        val host = Registry.get("ml.server.completion.host").asString()
-        val port = Registry.get("ml.server.completion.port").asInteger()
-
         const val FULL_LINE_TAIL_TEXT = "full-line"
         val GPT_ICON: Icon = PythonIcons.Python.Python
     }
