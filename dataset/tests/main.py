85a7d22fc262414b4f15cca5400c8102e885dff4
BarracudaPff <Kirill.Krylov@jetbrains.com>
2020-02-25 10:42:52 +0300

    upd launcher for tests

--- dataset/tests/main.py
+++ dataset/tests/main.py
@@ -1,10 +1,13 @@
+import os
 import unittest
+
+test_dir = os.path.dirname(os.path.abspath(__file__))
+tests_dir = os.path.join(test_dir, 'utils')
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
-    start_dir = 'tests'
-    suite = loader.discover(start_dir)
-    loader.discover('tests/utils')
+    suite = loader.discover(test_dir)
+    loader.discover(tests_dir)
 
     runner = unittest.TextTestRunner()
     runner.run(suite)
