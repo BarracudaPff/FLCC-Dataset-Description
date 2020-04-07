be9408ad504700ed05c626d3641f88cda0f3315d
BarracudaPff <Kirill.Krylov@jetbrains.com>
2020-04-07 14:19:50 +0300

    update dump and python formatters

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/language/formatters/DumpFormatter.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/language/formatters/DumpFormatter.kt
@@ -1,13 +1,13 @@
 package org.jetbrains.completion.full.line.language.formatters
 
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import org.jetbrains.completion.full.line.language.ElementFormatter
 
 class DumpFormatter : ElementFormatter {
     override fun condition(element: PsiElement): Boolean = true
 
-    override fun filter(element: PsiElement): Boolean? = null
+    override fun filter(element: PsiElement): Boolean? = element !is PsiFile && element.firstChild == null && element.text.isNotEmpty()
 
     override fun format(element: PsiElement): String = element.text
-
 }
--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/language/formatters/PythonCodeFormatter.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/language/formatters/PythonCodeFormatter.kt
@@ -1,11 +1,15 @@
 package org.jetbrains.completion.full.line.language.formatters
 
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.suggested.startOffset
 import org.jetbrains.completion.full.line.language.CodeFormatter
 import org.jetbrains.completion.full.line.language.formatters.python.*
 
 class PythonCodeFormatter : CodeFormatter(arrayOf(
         WhitespaceFormatter()
-        , NumericalFormatter()
+        , CommentFormatter()
+        , NumericalFormatter(false)
         , ImportFormatter()
         , ParenthesizedWithoutTuplesFormatter()
         , StringFormatter()
@@ -14,4 +18,14 @@ class PythonCodeFormatter : CodeFormatter(arrayOf(
         , SequenceFormatter()
         , ParameterListFormatter()
         , DumpFormatter()
-))
+)) {
+    override fun formatFinalElement(element: PsiElement, range: TextRange): String {
+        val text = element.text.slice(IntRange(0, (range.endOffset - element.startOffset - 1)))
+
+        println(text)
+        println(fixTabs(text))
+        println("|" + text + fixTabs(text) + "|")
+
+        return text + fixTabs(text)
+    }
+}
