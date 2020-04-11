4272c058d85312be1fc4b5fc848733f480b56023
BarracudaPff <Kirill.Krylov@jetbrains.com>
2020-04-11 16:01:23 +0300

    remove getInstance

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/language/ElementFormatter.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/language/ElementFormatter.kt
@@ -1,8 +1,6 @@
 package org.jetbrains.completion.full.line.language
 
-import com.intellij.lang.Language
 import com.intellij.psi.PsiElement
-import org.jetbrains.completion.full.line.language.supporters.PythonSupporter
 
 interface ElementFormatter {
     fun condition(element: PsiElement): Boolean
@@ -10,13 +8,4 @@ interface ElementFormatter {
     fun filter(element: PsiElement): Boolean?
 
     fun format(element: PsiElement): String
-
-    companion object {
-        fun getInstance(language: Language): LanguageMLSupporter? {
-            return when (language) {
-                Language.findLanguageByID("Python") -> PythonSupporter()
-                else -> null
-            }
-        }
-    }
 }
