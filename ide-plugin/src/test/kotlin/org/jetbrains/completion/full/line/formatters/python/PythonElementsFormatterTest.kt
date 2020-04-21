9a2dd73a8c606e39fa1c1a1312cef8c486b9fd3f
BarracudaPff <Kirill.Krylov@jetbrains.com>
2020-04-21 10:05:11 +0300

    add tests for latest fixes

--- ide-plugin/src/test/kotlin/org/jetbrains/completion/full/line/formatters/python/PythonElementsFormatterTest.kt
+++ ide-plugin/src/test/kotlin/org/jetbrains/completion/full/line/formatters/python/PythonElementsFormatterTest.kt
@@ -5,17 +5,52 @@ import org.junit.jupiter.api.Test
 
 class PythonElementsFormatterTest : PythonCodeFormatterTest() {
     @Test
-    fun `format numerical`() {
+    fun numerical() {
         testFile("test-elements/numerical.py")
     }
 
     @Test
-    fun `format strings`() {
+    fun `parameter list without closed bracket`() {
+        testCodeFragment("def nginx(_config ","def nginx(_config ", 18)
+    }
+
+    @Test
+    fun `function params without type`() {
+        testCodeFragment("def checkout_branch(branch):\n    return None", "def checkout_branch(branch):\n\treturn None")
+    }
+
+    @Test
+    fun `function params with type`() {
+        testCodeFragment("def checkout_branch(branch: str):\n    return None", "def checkout_branch(branch: str):\n\treturn None")
+    }
+
+    @Test
+    fun `function params with type and default value`() {
+        testCodeFragment("def checkout_branch(branch: str=\"master\"):\n    return None", "def checkout_branch(branch: str=\"master\"):\n\treturn None")
+    }
+
+    @Test
+    fun `not fully filled import`() {
+        testCodeFragment("import ", "import ")
+    }
+
+    @Test
+    fun `not fully filled from import`() {
+        testCodeFragment("from tqdm import ", "from tqdm import ")
+    }
+
+    @Test
+    fun `inside not fully filled from import`() {
+        testCodeFragment("from  import tqdm", "from ", 5)
+    }
+
+    @Test
+    fun strings() {
         testFile("test-elements/strings.py")
     }
 
     @Test
-    fun `format imports`() {
+    fun imports() {
         testFile("test-elements/imports.py")
     }
 }
