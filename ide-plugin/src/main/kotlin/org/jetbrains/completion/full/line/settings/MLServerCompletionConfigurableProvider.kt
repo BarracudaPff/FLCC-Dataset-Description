9cef59e26f9fd2231a3a2aae97874f06740c00cf
BarracudaPff <Kirill.Krylov@jetbrains.com>
2020-05-10 08:01:11 +0300

    get message from exception, pass methods

--- ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/settings/MLServerCompletionConfigurableProvider.kt
+++ ide-plugin/src/main/kotlin/org/jetbrains/completion/full/line/settings/MLServerCompletionConfigurableProvider.kt
@@ -3,11 +3,24 @@ package org.jetbrains.completion.full.line.settings
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.ConfigurableProvider
 import org.jetbrains.completion.full.line.GPTCompletionProvider
+import java.net.SocketTimeoutException
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeoutException
 
 class ServerCompletionConfigurableProvider : ConfigurableProvider() {
     override fun createConfigurable(): Configurable? {
-        return MLServerCompletionConfigurable(GPTCompletionProvider.getModels()) {
-            GPTCompletionProvider.getStatusCallback()
+        return MLServerCompletionConfigurable(
+                this::exceptionMessage,
+                { GPTCompletionProvider.getStatus() },
+                { GPTCompletionProvider.getModels() }
+        )
+    }
+
+    private fun exceptionMessage(it: Throwable): String {
+        return when (it) {
+            is SocketTimeoutException, is TimeoutException -> MLServerCompletionBundle.message("ml.server.completion.gpt.connection.timeout")
+            is ExecutionException -> it.cause?.localizedMessage ?: it.localizedMessage
+            else -> it.localizedMessage
         }
     }
 }
